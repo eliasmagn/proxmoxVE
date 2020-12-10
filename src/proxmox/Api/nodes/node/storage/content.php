8aa231c4a040909382fb1d73a3ed7c45a2665cb4
eliasmagn <elias@eliashaisch.de>
2020-12-10 20:07:00 +0100

    add nodes/{node}/storage/{storage}/content/{volume} functionality , first attempt

--- src/proxmox/Api/nodes/node/storage/content.php
+++ src/proxmox/Api/nodes/node/storage/content.php
@@ -1,15 +1,16 @@
 <?php
 /**
  * @copyright 2020 Daniel Engelschalk <hello@mrkampf.com>
- * @copyright 2020 Elias Haisch <elias@eliashaisch.de>
  */
 namespace proxmox\Api\nodes\node\storage;
+
 use GuzzleHttp\Client;
+use proxmox\Api\nodes\node\storage\content\volume;
 use proxmox\Helper\connection;
 
 /**
- * Class version
- * @package proxmox\api\nodes\node
+ * Class content
+ * @package proxmox\api\nodes\node\storage
  */
 class content
 {
@@ -21,38 +22,70 @@ class content
      * content constructor.
      * @param $httpClient Client
      * @param $apiURL string
-     * @param $ticket string
+     * @param $cookie mixed
      */
     public function __construct($httpClient,$apiURL,$cookie){
         $this->httpClient = $httpClient; //Save the http client from GuzzleHttp in class variable
-        $this->apiURL = $apiURL; //Save api url in class variable and change this to current api path
+        $this->apiURL = $apiURL.'volume/'; //Save api url in class variable and change this to current api path
         $this->cookie = $cookie; //Save auth cookie in class variable
-
     }
 
-
-    /**
-     * Path: /nodes/{node}/storage/{storage}/content/{volume}
-     * Get volume attributes
-     * @url https://pve.proxmox.com/pve-docs/api-viewer/index.html#/nodes
-     * @param $string
-     * @return mixed|null
-     */
-    public function volume($volume){
-        return new volume($this->httpClient,$this->apiURL.$volume.'/',$this->cookie);
-    }
 
     /**
      * GET
      */
 
     /**
-     * Path: /nodes/{node}/storage
-     * Get status for all datastores.
-     * @url https://pve.proxmox.com/pve-docs/api-viewer/index.html#/nodes/{node}/storage
-     * @return mixed|null
+     * Get volume attributes
+     * @url https://pve.proxmox.com/pve-docs/api-viewer/index.html#/nodes/{node}/storage/{storage}/content/{volume}
+     * @return mixed
      */
-    public function get(){
-        return connection::processHttpResponse(connection::getAPI($this->httpClient,$this->apiURL,$this->cookie));
+    public function getvolume(){
+        return new volume($this->httpClient,$this->apiURL,$this->cookie);
     }
+
+    /**
+     * POST
+     */
+
+    /**
+     *  Path: /nodes/{node}/storage/{storage}/content/{volume}
+	 * Copy a volume. This is experimental code - do not use.
+     * @url https://pve.proxmox.com/pve-docs/api-viewer/index.html#/nodes/{node}/storage/{storage}/content/{volume}
+     * @param $target_node string 
+     * @return string
+     */
+    public function postVolume($target_node=""){
+        return connection::processHttpResponse(connection::postAPI($this->httpClient,$this->apiURL,$this->cookie,$target_node));
+    }
+
+    /**
+     * PUT
+     */
+
+    /**
+     * Resource Tree
+     * Update volume attributes
+     * @url https://pve.proxmox.com/pve-docs/api-viewer/index.html#/nodes/{node}/storage/{storage}/content/{volume}
+     * @param $notes string
+     * @return null
+     */
+    public function putVolume($notes=""){
+        return connection::processHttpResponse(connection::putAPI($this->httpClient,$this->apiURL,$this->cookie,$params));
+    }
+
+    /**
+     * DELETE
+     */
+
+    /**
+     * Destroy the vm (also delete all used/owned volumes).
+     * @url https://pve.proxmox.com/pve-docs/api-viewer/index.html#/nodes/{node}/storage/{storage}/content/{volume}
+     * @param $delay int
+     * @return string
+     */
+    public function deleteVolume($delay="2"){
+        return connection::processHttpResponse(connection::deleteAPI($this->httpClient,$this->apiURL,$this->cookie,$delay));
+    }
+
 }
