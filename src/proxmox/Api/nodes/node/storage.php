f831c30256c5dec8d287e67edb5e48ca5148bdee
eliasmagn <elias@eliashaisch.de>
2020-12-10 14:43:29 +0100

    first atttempt to add node/storage  /api2/json/nodes/{node}/storage/{storage}

---
+++ src/proxmox/Api/nodes/node/storage.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @copyright 2020 Daniel Engelschalk <hello@mrkampf.com>
+ * @copyright 2020 Elias Haisch <elias@eliashaisch.de>
+ */
+namespace proxmox\Api\nodes\node;
+
+use GuzzleHttp\Client;
+use proxmox\Helper\connection;
+
+/**
+ * Class version
+ * @package proxmox\api\nodes\node
+ */
+class nodestorage
+{
+    private $httpClient, //The http client for connection to proxmox
+        $apiURL, //API url
+        $cookie; //Proxmox auth cookie
+
+    /**
+     * nodestorage constructor.
+     * @param $httpClient Client
+     * @param $apiURL string
+     * @param $ticket string
+     */
+    public function __construct($httpClient,$apiURL,$cookie){
+        $this->httpClient = $httpClient; //Save the http client from GuzzleHttp in class variable
+        $this->apiURL = $apiURL.'/api2/json/nodes/node/storage/'; //Save api url in class variable and change this to current api path
+        $this->cookie = $cookie; //Save auth cookie in class variable
+
+    }
+}
+
+/* TODO HERE
+*
+*prunebackup
+*
+*rrd
+*
+*rrddata
+*
+*status
+*
+*upload
+*
+*/
+
+    /**
+     * Directory index
+     * @url https://pve.proxmox.com/pve-docs/api-viewer/index.html#/nodes
+     * @param $storage string
+     * @return mixed
+     */
+    public function storage($storage){
+        return new storage($this->httpClient,$this->apiURL.$storage.'/',$this->cookie);
+    }
+
+
+    /**
+     * GET
+     */
+
+    /**
+     * Path: /nodes/{node}/storage
+     * Get status for all datastores.
+     * @url https://pve.proxmox.com/pve-docs/api-viewer/index.html#/nodes/{node}/storage
+     * @return mixed|null
+     */
+    public function get(){
+        return connection::processHttpResponse(connection::getAPI($this->httpClient,$this->apiURL,$this->cookie));
+    }
